faeb4d0701c8c772982f96e5c4635875b8f466ef
0lbap <pablo.laviron@etu.umontpellier.fr>
2024-03-26 23:53:34 +0100

    Offer page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { LoginComponent } from './login/login.component'
 import { SignupComponent } from './signup/signup.component'
+import { OfferComponent } from './offer/offer.component'
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'offer/:id', component: OfferComponent },
 ]
---
+++ src/app/comment.interface.ts
@@ -0,0 +1,9 @@
+export interface Comment {
+  id: number
+  offerId: number
+  author: string
+  content: string
+  stars: number
+  date: Date
+  pfpUrl: string
+}
