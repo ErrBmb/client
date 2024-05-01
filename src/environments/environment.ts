31f20e0beedabc971d8943b68f91ac9904c78a93
0lbap <pablo.laviron@etu.umontpellier.fr>
2024-05-01 17:08:08 +0200

    chore: added API_BASE_URL environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   MAPBOX_TOKEN:
     'pk.eyJ1Ijoic3ZpYWwiLCJhIjoiY2x2bnRoemRhMGU1ajJrcDFwN3FwZ3N3cCJ9.xaG1_EO4PMfhjOlc3lyrSA',
+  API_BASE_URL: 'http://my-prod-url', // TODO: replace with production URL
 }
