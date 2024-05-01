b4a4d647437e3e9a3b5a03b00fd2950a3302354e
0lbap <pablo.laviron@etu.umontpellier.fr>
2024-05-01 17:52:02 +0200

    Added the production api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   MAPBOX_TOKEN:
     'pk.eyJ1Ijoic3ZpYWwiLCJhIjoiY2x2bnRoemRhMGU1ajJrcDFwN3FwZ3N3cCJ9.xaG1_EO4PMfhjOlc3lyrSA',
-  API_BASE_URL: 'http://my-prod-url', // TODO: replace with production URL
+  API_BASE_URL: 'https://backend-01o9.onrender.com',
 }
